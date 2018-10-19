b7faeb479f18b7d6b0118246d611d4a85835f0f2
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2018-10-19 14:30:35 -0400

    Added graphics translate

--- app/scripts/EmptySVGTrack.js
+++ app/scripts/EmptySVGTrack.js
@@ -3,9 +3,24 @@ import SVGTrack from './SVGTrack';
 class EmptySVGTrack extends SVGTrack {
   constructor(svgElement) {
     super(svgElement);
+    this.gEmpty = this.gMain
+      .append('g');
   }
 
+  // draw() {
+  //   console.log('draw?')
+  //   // TODO
+  // }
 
+  zoomed(newXScale, newYScale) {
+    console.log('zoomed');
+    //super.zoomed(newXScale, newYScale
+    const x = newXScale(0);
+    const y = newYScale(0);
+    this.gEmpty.attr('transform', `translate(${x}, ${y})`);
+
+    this.draw();
+  }
   // draw() {
   //   // this.axis.scale(this._yScale);
   //   // this.gAxis.call(this.axis);
