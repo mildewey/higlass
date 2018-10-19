61b2a35eb13d04ef88f3b95c77351aec3a2d913b
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2018-10-19 14:34:21 -0400

    And now it scales

--- app/scripts/EmptySVGTrack.js
+++ app/scripts/EmptySVGTrack.js
@@ -7,39 +7,13 @@ class EmptySVGTrack extends SVGTrack {
       .append('g');
   }
 
-  // draw() {
-  //   console.log('draw?')
-  //   // TODO
-  // }
-
   zoomed(newXScale, newYScale) {
-    console.log('zoomed');
-    //super.zoomed(newXScale, newYScale
     const x = newXScale(0);
     const y = newYScale(0);
-    this.gEmpty.attr('transform', `translate(${x}, ${y})`);
-
+    const dx = newXScale(1) - x;
+    this.gEmpty.attr('transform', `translate(${x}, ${y}) scale(${dx})`);
     this.draw();
   }
-  // draw() {
-  //   // this.axis.scale(this._yScale);
-  //   // this.gAxis.call(this.axis);
-  //   //
-  //   // return this;
-  // }
-  //
-  // zoomed(newXScale, newYScale) {
-  //   super.zoomed(newXScale, newYScale);
-  //
-  //   this.draw();
-  // }
-  //
-  // zoomed(newXScale, newYScale) {
-  //   this.xScale(newXScale);
-  //   this.yScale(newYScale);
-  //
-  //   this.draw();
-  // }
 }
 
 export default EmptySVGTrack;
